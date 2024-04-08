179374f402e76082f49a2b4d52c4fa749196ff98
osj.beaver <zasqw456@gmail.com>
2024-04-08 22:27:38 +0900

    도키도키 간식드리미 문제 해결

---
+++ src/main/kotlin/osj/beaver/baekjoon/stackqueuedeque/P12789.kt
@@ -0,0 +1,49 @@
+package osj.beaver.baekjoon.stackqueuedeque
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.Stack
+import java.util.StringTokenizer
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val n = reader.readLine().toInt()
+
+    val array = IntArray(n)
+    val stringTokenizer = StringTokenizer(reader.readLine())
+
+    repeat(n) {
+        val student = stringTokenizer.nextToken().toInt()
+        array[it] = student
+    }
+
+    val stack = Stack<Int>()
+    var order = 1
+    var index = 0
+    while(index != n) {
+        if (array[index] == order) {
+            order++
+        } else {
+            if (stack.isNotEmpty() && stack.peek() == order) {
+                stack.pop()
+                order++
+                index--
+            } else {
+                stack.push(array[index])
+            }
+        }
+        index++
+    }
+
+    for (i in 0 until stack.size) {
+        if (stack.pop() == order) {
+            order++
+        } else {
+            println("Sad")
+            return
+        }
+    }
+
+    println("Nice")
+    reader.close()
+}
